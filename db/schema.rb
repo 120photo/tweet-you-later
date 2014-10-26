db7c84c0051843922571f019efd913f6853db852
Jose Juarez <120photo@gmail.com>
2014-10-26 02:14:18 -0700

    added image to users model, setup mailer for dev / production if needed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141026063519) do
+ActiveRecord::Schema.define(version: 20141026090954) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20141026063519) do
     t.datetime "updated_at"
     t.string   "provider"
     t.string   "uid"
+    t.string   "image"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
