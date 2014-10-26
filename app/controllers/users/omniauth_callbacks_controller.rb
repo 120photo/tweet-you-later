ff14d47c87330ee67702504e8cb6a1f03ddc9054
Jose Juarez <120photo@gmail.com>
2014-10-26 01:22:00 -0700

    twitter authentication now works, thank you Mr. Bates

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,10 +3,9 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication
-      set_flash_message(:notice, :success, :kind => "Twitter") if is_navigational_format?
+      sign_in_and_redirect @user, notice: "Login Worked!"
     else
-      session["devise.twitter_date"] = request.env["omniauth.auth"]
+      session["devise.user_attributes"] = @user.attributes
       redirect_to new_user_registration_url
     end
   end
